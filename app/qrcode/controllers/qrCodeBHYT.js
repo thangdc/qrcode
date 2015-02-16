f231c97c0a8b381acd7e262e4734574bf658dc4a
Dinh Cong Thang <dinhcongthang113@hotmail.com>
2015-02-16 20:54:59 +0700

    Resest when decode qrcode

--- app/qrcode/controllers/qrCodeBHYT.js
+++ app/qrcode/controllers/qrCodeBHYT.js
@@ -8,6 +8,8 @@
     $scope.decode = function () {
         $scope.loading = true;
         $scope.error = '';
+		$scope.result = {};
+		$scope.image = '';
         qrCodeServices.decodeQRCodeBHYT($scope.image).$promise.then(
             function (response) {
                 $scope.loading = false;
